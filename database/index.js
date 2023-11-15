96a7788c6d4f637bc38f5aa66abdb440515e1352
Minh Nguyen <mnyen350@gmail.com>
2023-11-15 02:30:22 -0500

    remove redundant error

--- database/index.js
+++ database/index.js
@@ -6,10 +6,7 @@ const Ticket = require("./models/ticket.model");
 function startMongoose()
 {
     const uri = process.env.MONGO_CONNECTION;
-    if (!uri) {
-        throw new Error('Variable MONGO_CONNECTION was not specified in .env file');
-    }
-
+    
     mongoose.set('runValidators', true);
     mongoose.pluralize(null); // disable mongo pluralizing collections
     mongoose.connect(uri);
