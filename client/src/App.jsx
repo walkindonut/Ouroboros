0d4f8f0d4c0879e5467c5c12d6440c50ac6cdbe1
Joghtapour <K1_joghtapour@yahoo.com>
2023-11-20 11:59:05 -0500

    Html primal design
    this is a desgin for Html

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,26 +8,28 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <div>
+
+<header>
+    <h2> Incident Management </h2>
+</header>  
+<nav>  
+    <ul>
+        <li>            <a href="home.html">Home Page  </a></li>
+        <li>            <a href="Profile.html">Profile</a> </li>
+        <li>            <a href="Ticket.html">Ticket Management</a></li>
+    </ul>
+</nav>
+
+
+<main> <p>yhuh</p>
+</main>
+
+<footer>
+   Copyright &copy; 2023   
+    <a href="mailto:rcaraba@centennialcollege.ca">kjoghtap@centennialcollege.ca</a>
+</footer>
+</div>
     </>
   )
 }
