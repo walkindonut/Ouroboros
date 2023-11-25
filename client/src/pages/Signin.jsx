63ab5da34cabe12b97cc5bd67ac1e3b2a7d8b5ce
Joghtapour <K1_joghtapour@yahoo.com>
2023-11-24 20:08:04 -0500

    created login/sign up page
    created login/sign up page

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,27 +1,19 @@
-import React from 'react'
+import React from 'react';
+import './Signin.css'; // Link to your CSS file
 
 function Signin() {
   return (
     <div className="login">
-          <h1>Log In</h1>
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            id=""
-          />
-          <br />
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            id=""
-          />
-          <button className="loginButton">
-            Log In Account
-          </button>
-        </div>
-  )
+      <a href='/'><img className="logo" src="/Logo/1.png" /></a> 
+
+      <h1>Log In</h1>
+      <input type="email" name="email" placeholder="Email" id="" />
+      <br />
+      <input type="password" name="password" placeholder="Password" id="" />
+      <button className="loginButton">Log In Account</button>
+      <a href="/signup"><button className="CreateButton">Create an Account</button></a>
+    </div>
+  );
 }
 
-export default Signin
+export default Signin;
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import './Signup.css'; // Link to your CSS file
 
 function Signup() {
   return (
-    <div>Signup</div>
-  )
+    <div className="SignupPage">
+      <a href='/'><img className="logo" src="/Logo/1.png" /></a> 
+
+      <h1>Sign Up</h1>
+      <input type="text" name="fullname" placeholder="Full Name" id="" />
+      <input type="number" name="number" placeholder="Phone number" id="" />
+      <input type="email" name="email" placeholder="Email" id="" />
+      <br />
+      <input type="password" name="password" placeholder="Password" id="" />
+      <button className="loginButton">Create an Account</button>
+    </div>
+  );
 }
+
 
 export default Signup
